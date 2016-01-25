bfaa18c870b124c4d020a82188dd82c4335e55d3
Diarmuid Murphy <diarmuid.com@gmail.com>
2016-01-25 05:44:46 +0000

    refactor zipcode function

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,24 @@
 require "csv"
+
+def clean_zipcode(zipcode)
+    if zipcode.nil?
+        "00000"
+    elsif zipcode.length < 5
+        zipcode.rjust(5, "0")
+    elsif zipcode.length > 5
+        zipcode[0..4]
+    else
+        zipcode
+    end
+end
+
 puts "EventManager Initialized!"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
     name = row[:first_name]
-    zipcode = row[:zipcode]
+    
+    zipcode = clean_zipcode(row[:zipcode])
+    
     puts "#{name} #{zipcode}"
 end
